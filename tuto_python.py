a87bfb7308e850f43668b8f1289fb87eac32ad74
yoni <yoni.amarv@gmail.com>
2019-01-29 11:50:10 +0200

    first commit

---
+++ tuto_python.py
@@ -0,0 +1,119 @@
+# somme = input("quelle est la somme ? ")
+# print(somme)
+
+# fruit = ['banane', 'poire', 'citron']
+# print(fruit[-1])
+
+# testlist = [1,2,3,4,5]
+# print(testlist, 'length is', len(testlist))
+
+# test = 10 % 3
+# print(test)
+
+# mon chien est minion
+# test = ['mon', 'chien', 'est', 'minion']
+# print('le mot est: ', len(test[1])
+
+# txt = "hello world"
+# x = txt.strip()
+
+# txt = "hello world"
+# x = txt[2:5]
+# print(x)
+
+# txt = "hello world"
+# txt = txt.upper()
+# print(txt)
+
+# txt = "HELLO WORLD"
+# txt = txt.lower()
+# print(txt)
+
+# txt = "Hello World"
+# txt = txt.replace("H", "J")
+# print(txt)
+
+# fruits = ["apple", "banana", "cherry"]
+# fruits[0] = "kiwi"
+
+# fruits = ["apple", "banana", "cherry"]
+# fruits.append("orange")
+
+# fruits = ["apple", "banana", "cherry"]
+# fruits.insert(1,"lemon")
+
+# fruits = ["apple", "banana", "cherry"]
+# fruits.remove("banana")
+
+# fruits = ("apple", "banana", "cherry")
+# print(len(fruits))
+
+# fruits = {"apple", "banana", "cherry"}
+# fruits.add("orange")
+
+# fruits = {"apple", "banana", "cherry"}
+# more_fruits = ["orange", "mango", "grapes"]
+# fruits.update(more_fruits)
+# print(fruits)
+
+# fruits = {"apple", "banana", "cherry"}
+# fruits.discard("banana")
+# fruits.add("kiwi")
+# fruits.remove("apple")
+# print(fruits)
+
+# ----------------------------
+# car =	{
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# print(car.get("model"))
+
+# --------------------------
+# car =	{
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+
+# car["year"] = 2018
+
+# ----------------------------
+# car =	{
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+
+# car["color"] = "red"
+# print(car)
+
+
+
+# ------------------------------
+# car =	{
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+
+# car.pop("model")
+# print(car)
+
+# -------------------------------
+# car =	{
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+
+# car.clear()
+
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+
